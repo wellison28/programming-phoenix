7505aa8f9ea71aad5b92229dba6304f334bb5236
Wellison Pereira <wellison.pereira@gencomm.com.br>
2019-11-26 10:19:28 -0300

    create page to show user

--- lib/rumbl_web/controllers/user_controller.ex
+++ lib/rumbl_web/controllers/user_controller.ex
@@ -6,5 +6,10 @@ defmodule RumblWeb.UserController do
 	def index(conn, _params) do
 		users = Accounts.list_users() 
 		render(conn, "index.html", users: users)
-	end 
+	end
+
+	def show(conn, %{"id" => id}) do
+		user = Accounts.get_user(id)
+		render(conn, "show.html", user: user)
+	end
 end
