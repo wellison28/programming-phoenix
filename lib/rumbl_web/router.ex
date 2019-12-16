a9d47b6b024bef174d24b775eb3c8b5d825742a2
Wellison Pereira <wellison28@gmail.com>
2019-12-16 14:19:18 -0300

    add authentication page

--- lib/rumbl_web/router.ex
+++ lib/rumbl_web/router.ex
@@ -15,14 +15,11 @@ defmodule RumblWeb.Router do
   end
 
   scope "/", RumblWeb do
+    # Use the default browser stack
     pipe_through :browser
 
     get "/", PageController, :index
     resources "/users", UserController, only: [:index, :show, :new, :create]
+    resources "/sessions", SessionController, only: [:new, :create, :delete]
   end
-
-  # Other scopes may use custom stacks.
-  # scope "/api", RumblWeb do
-  #   pipe_through :api
-  # end
 end
