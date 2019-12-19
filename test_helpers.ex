133c21df2b48407c678bbeb59c15cddd3a4ad9ec
Wellison Souza <wellison28@gmail.com>
2019-12-19 16:59:33 -0300

    add tests

---
+++ test_helpers.ex
@@ -0,0 +1,24 @@
+" Press ? for help
+
+.. (up a dir)
+/Users/wellison.pereira/projects/rumbl/
+▸  _build/
+▸  assets/
+▸  config/
+▸  deps/
+▸  lib/
+▸  priv/
+▾  test/
+  ▸  rumbl/
+  ▸  rumbl_web/
+  ▾  support/
+       channel_case.ex
+       conn_case.ex
+       data_case.ex
+     test_helper.exs
+   accounts_test.exs
+   asd
+   mix.exs
+   mix.lock
+   new
+   README.md
